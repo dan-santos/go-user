e5dbb03f23726402e11d0ccf20d005af3f75ec24
Dan <santos.danielfd@gmail.com>
2024-01-29 19:17:28 -0300

    implementation of simply logger using zaplogger pkg

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log/slog"
 
+	"github.com/dan-santos/go-user/src/configs/logger"
 	"github.com/dan-santos/go-user/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	logger.Info("Initiating User API statup")
 	err := godotenv.Load()
 	if err != nil {
 		slog.Error("Error loading .env file")
--- src/controller/create.go
+++ src/controller/create.go
@@ -1,21 +1,33 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/dan-santos/go-user/src/configs/logger"
 	"github.com/dan-santos/go-user/src/configs/validation"
 	"github.com/dan-santos/go-user/src/controller/dto"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func Create(c *gin.Context) {
 	var userRequest dto.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err, zap.String("path", "createUser"))
 		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
+		return
 	}
 
-	fmt.Println(userRequest)
+	response := dto.UserResponse{
+		ID: "test",
+		Email: userRequest.Email,
+		Name: userRequest.Name,
+		Age: userRequest.Age,
+	}
+
+	logger.Info("User created successfully")
+	c.JSON(http.StatusOK, response)
 }
